61a18b46b57b0fb47e63bb52af5e4e6234279e61
Ruben Corral <rubencorralsola@gmail.com>
2021-09-29 16:21:21 +0200

    home still to work

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,14 +4,12 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <Container>
-  
-    <Col xs="auto" className="my-1" id="login">
-    <Link button to={"/login"}> <b>Login</b></Link>
-
-      
-    </Col>
-
-</Container>
+      <Row xs={1} md={1} lg={2} xl={3}> 
+        <Col xs="auto" className="my-1" id="login">
+          <Link to={"/login"}><Button>Login</Button></Link>
+        </Col>
+      </Row>
+    </Container>
 
   );
 }
