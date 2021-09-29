232d6d0b0c185aec8edddc7b5be41b8447191732
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-09-29 15:53:36 +0200

    home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,16 @@
 import {Container,Row,Col,Form,Button,InputGroup, FormControl} from "react-bootstrap"
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <Container>
-    <Form>
-  <Row className="align-items-center">
-    <Col sm={3} className="my-1">
-      <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-       Name
-      </Form.Label>
-      <Form.Control id="inlineFormInputName" placeholder="User Name" />
-    </Col>
-    <Col sm={3} className="my-1">
-      <Form.Label htmlFor="inlineFormInputGroupUsername" visuallyHidden>
-       Username
-      </Form.Label>
-      <InputGroup>
-        <FormControl id="inlineFormInputGroupUsername" placeholder="Password" />
-      </InputGroup>
-    </Col>
-    
-    <Col xs="auto" className="my-1">
-      <Button type="submit" id="login"><b>LogIn</b></Button>
+  
+    <Col xs="auto" className="my-1" id="login">
+    <Link button to={"/login"}> <b>Login</b> </Link>
+
       
     </Col>
-  </Row>
-</Form>
+
 </Container>
 
   );
