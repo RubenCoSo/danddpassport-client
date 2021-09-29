2ab1b4ee75c778f2b12e683a59e751940e4e94d4
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-09-29 16:04:48 +0200

    homePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,41 +1,16 @@
 import {Container,Row,Col,Form,Button,InputGroup, FormControl} from "react-bootstrap"
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <Container>
-<<<<<<< Updated upstream
-    <Form>
-  <Row className="align-items-center">
-    <Col sm={3} className="my-1">
-      <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-       Mail
-      </Form.Label>
-      <Form.Control id="inlineFormInputName" placeholder="Email" />
-    </Col>
-    <Col sm={3} className="my-1">
-      <Form.Label htmlFor="inlineFormInputGroupUsername" visuallyHidden>
-       Password
-      </Form.Label>
-      <InputGroup>
-        <FormControl id="inlineFormInputGroupUsername" placeholder="Password" />
-      </InputGroup>
-    </Col>
-    <Col sm={3} className="my-1">
-      <Form.Label htmlFor="inlineFormInputGroupUsername" visuallyHidden>
-       Name
-      </Form.Label>
-      <InputGroup>
-        <FormControl id="inlineFormInputGroupUsername" placeholder="Name" />
-      </InputGroup>
-    </Col>
-=======
->>>>>>> Stashed changes
-    
-    <Col xs="auto" className="my-1">
-      <Button type="submit" id="login"><b>LogIn</b></Button>
+  
+    <Col xs="auto" className="my-1" id="login">
+    <Link button to={"/login"}> <b>Login</b></Link>
+
       
     </Col>
- 
+
 </Container>
 
   );
