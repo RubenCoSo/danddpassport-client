264395fd0cf53b2ad1625bb3defbc94d3bbc7962
Ruben Corral <rubencorralsola@gmail.com>
2021-09-29 15:42:10 +0200

    Login page done

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import { useState, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./../context/auth.context";
-
+import { Container, Row, Form, Col, Button } from "react-bootstrap"
 const API_URL = process.env.REACT_APP_API_URL;
 
 
@@ -37,23 +37,50 @@ function LoginPage(props) {
   };
   
   return (
-    <div className="LoginPage">
-      <h1>Login</h1>
+    // <div className="LoginPage">
+    //   <h1>Login</h1>
 
-      <form onSubmit={handleLoginSubmit}>
-        <label>Email:</label>
-        <input type="text" name="email" value={email} onChange={handleEmail} />
+    //   <form onSubmit={handleLoginSubmit}>
+    //     <label>Email:</label>
+    //     <input type="text" name="email" value={email} onChange={handleEmail} />
 
-        <label>Password:</label>
-        <input type="password" name="password" value={password} onChange={handlePassword} />
+    //     <label>Password:</label>
+    //     <input type="password" name="password" value={password} onChange={handlePassword} />
 
-        <button type="submit">Login</button>
-      </form>
-      { errorMessage && <p className="error-message">{errorMessage}</p> }
+    //     <button type="submit">Login</button>
+    //   </form>
+    //   { errorMessage && <p className="error-message">{errorMessage}</p> }
 
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
-    </div>
+    //   <p>Don't have an account yet?</p>
+    //   <Link to={"/signup"}> Sign Up</Link>
+    // </div>
+
+  <Container>
+    <Row sm={1} md={1}>
+      <Col>
+        <Form onSubmit={handleLoginSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name="email" value={email} onChange={handleEmail} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" name="password" value={password} onChange={handlePassword} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        { errorMessage && <p className="error-message">{errorMessage}</p> }
+        <p>Don't have an account yet?</p>
+        <Link to={"/signup"}> Sign Up</Link>
+      </Col>
+    </Row>
+  </Container>
+
+    
   )
 }
 
