e3144bc171e59d242cc36efd6b4ae9230dcc10c0
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-10-01 09:40:12 +0200

    class and race

--- src/pages/CreateNewCharacter.js
+++ src/pages/CreateNewCharacter.js
@@ -71,34 +71,57 @@ export default function CreateNewCharacter(props) {
               />
             </InputGroup>
           </Col>
-          <Col xs="auto">
-            <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
-              Race
-            </Form.Label>
-            <InputGroup className="mb-2">
-              <FormControl
-                name="race"
-                value={race}
-                onChange={(e) => setRace(e.target.value)}
-                id="inlineFormInputGroup"
-                placeholder="Race"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs="auto">
-            <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
-              Class
-            </Form.Label>
-            <InputGroup className="mb-2">
-              <FormControl
-                name="characterClass"
-                value={characterClass}
-                onChange={(e) => setCharacterClass(e.target.value)}
-                id="inlineFormInputGroup"
-                placeholder="Class"
-              />
-            </InputGroup>
-          </Col>
+          <Form>
+  <Row className="align-items-center">
+    <Col xs="auto" className="my-1">
+      <Form.Label
+        className="me-sm-2"
+        htmlFor="inlineFormCustomSelect"
+        visuallyHidden
+      >
+        Preference
+      </Form.Label>
+      <Form.Select className="me-sm-2" id="inlineFormCustomSelect">
+        <option value="0">Choose...</option>
+        <option value="1">Dragonborn</option>
+        <option value="2">Dwarf</option>
+        <option value="3">Elf</option>
+        <option value="4">Gnome</option>
+        <option value="5">Half-Helf</option>
+        <option value="6">Halfing</option>
+        <option value="7">Half-Orc</option>
+        <option value="8">Human</option>
+        <option value="9">Tiefling</option>
+        <option value="10">Orc</option>
+      </Form.Select>
+    </Col>
+  </Row>
+</Form>
+           <Col xs="auto" className="my-1">
+      <Form.Label
+        className="me-sm-2"
+        htmlFor="inlineFormCustomSelect"
+        visuallyHidden
+      >
+        Preference
+      </Form.Label>
+      <Form.Select className="me-sm-2" id="inlineFormCustomSelect">
+        <option value="0">Choose...</option>
+        <option value="1">Barbarian</option>
+        <option value="2">Bard</option>
+        <option value="3">Cleric</option>
+        <option value="4">Druid</option>
+        <option value="5">Figther</option>
+        <option value="6">Monk</option>
+        <option value="7">Paladin</option>
+        <option value="8">Ranger</option>
+        <option value="9">Rogue</option>
+        <option value="10">Sorcerer</option>
+        <option value="11">Warlock</option>
+        <option value="12">Wizard</option>
+        <option value="13">Artificer</option>
+      </Form.Select>
+    </Col>
 
           <Col xs="auto">
             <Button type="submit" className="mb-2">
