b7c3235e2b65c27c50aa916909b67c89f2ff73ac
Ruben Corral <rubencorralsola@gmail.com>
2021-10-06 17:25:02 +0200

    cleaning archives

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import ProjectListPage from "./pages/ProjectListPage";
-import ProjectDetailsPage from "./pages/ProjectDetailsPage";
-import EditProjectPage from "./pages/EditProjectPage";
-
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";    // <== IMPORT
@@ -47,10 +43,7 @@ function App() {
 
         
         {/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
-        
-        <PrivateRoute exact path="/projects" component={ProjectListPage} />
-        <PrivateRoute exact path="/projects/:id" component={ProjectDetailsPage} />
-        <PrivateRoute exact path="/projects/edit/:id" component={EditProjectPage} />
+
         
         <AnonRoute exact path="/signup" component={SignupPage} />
         <AnonRoute exact path="/login" component={LoginPage} />
