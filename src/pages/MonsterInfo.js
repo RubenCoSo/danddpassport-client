1a43491a4cac420200874dfd98f6aa5c309ffe37
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-10-07 00:49:21 +0200

    monster info

--- src/pages/MonsterInfo.js
+++ src/pages/MonsterInfo.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const DNDAPI = "https://www.dnd5eapi.co/api/";
 
 function MonsterInfo(props) {
   const [monster, setMonster] = useState();
@@ -14,7 +14,7 @@ function MonsterInfo(props) {
 
   useEffect(() => {
     axios
-      .get(`${API_URL}/monster/${monsterId}`, {
+      .get(`${DNDAPI}monsters/${monster}`, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => {
@@ -26,18 +26,42 @@ function MonsterInfo(props) {
 
   return monster ? (
     <Container class="info">
-      <Row>
-        <Col>
-          <h1>Monster info</h1>
-        </Col>
-      </Row>
+      <Row></Row>
       <Row>
         <Col sm={1} md={1}>
-          <Image src={monster.image} id="profileImage"></Image>
-        </Col>
-        <Col sm={1} md={1}>
-          <h2> Name: {monster.monsterName}</h2>
-          <h4></h4>
+          <h2>
+            {" "}
+            <u> Name: {monster.name} </u>
+          </h2>
+          <p>Size: {monster.size}</p>
+          <p>Type: {monster.type}</p>
+          <p>Subtype: {monster.subtype}</p>
+          <p>Alignment: {monster.alignment}</p>
+          <p>Armor_class: {monster.armor_class}</p>
+          <p>Hit_points: {monster.hit_points}</p>
+          <p>Hit_dice: {monster.hit_dice}</p>
+          <p>Speed Walk: {monster.speed.walk}</p>
+          <h2>
+            <u> Stats </u>
+          </h2>
+          <p>Strength: {monster.strength}</p>
+          <p>Dexterity: {monster.dexterity}</p>
+          <p>Constitution: {monster.constitution}</p>
+          <p>Intelligence: {monster.intelligence}</p>
+          <p>Wisdom: {monster.wisdom}</p>
+          <p>Charisma: {monster.charisma}</p>
+          <h2>
+            <u> Actions </u>
+          </h2>
+          {monster.actions.map((action) => {
+            return(
+            <>
+              <p>Name: {action.name}</p>
+              <p>Description: {action.desc}</p>
+              <p>Attack Bonus: {action.attack_bonus}</p>
+  
+            </>)
+          })}
         </Col>
       </Row>
     </Container>
